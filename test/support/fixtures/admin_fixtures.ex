ff6138ecc2366fe41478807005197a90253a6cc5
lundjrl <jrlbidamin@gmail.com>
2023-08-27 20:47:44 -0400

    Init Elixir project

---
+++ test/support/fixtures/admin_fixtures.ex
@@ -0,0 +1,42 @@
+defmodule UsersApi.AdminFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `UsersApi.Admin` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+
+      })
+      |> UsersApi.Admin.create_user()
+
+    user
+  end
+
+  @doc """
+  Generate a unique user email.
+  """
+  def unique_user_email, do: "some email#{System.unique_integer([:positive])}"
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        name: "some name",
+        address: "some address",
+        role: "some role",
+        email: unique_user_email()
+      })
+      |> UsersApi.Admin.create_user()
+
+    user
+  end
+end
